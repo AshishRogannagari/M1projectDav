5db9a8f8aefce67a3293f0453485ecad96ac4ea2
Rogannagari Ashish <125127793+AshishRogannagari@users.noreply.github.com>
2023-10-25 22:04:47 -0400

    Add files via upload

---
+++ package/part2.py
@@ -0,0 +1,15 @@
+class P2:
+    def __init__(self,df):
+        self.df = df
+    
+    def data_sum(self):
+        # rows and columns 
+        # numerical ah catogticals ???
+        # link 
+        print("Data analysis and customer segments")
+        print("–in your analysis make sure you define the metrics ")
+        print("– look at total sales, drivers of sales, where the highest sales are coming from etc.")
+        print("Explore the data, create charts and graphs as well as noting any interesting trends and/or insights you find.")
+        print("The Data set is about to sales transcations")           
+        return 
+    
